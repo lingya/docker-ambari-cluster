269f230fa9ef2d526a134b120e06fe0ebcc56579
Luciano Resende <lresende@apache.com>
2015-02-10 21:40:17 -0800

    Ambari server container image
    Used to boostrap Ambari and setup HDP cluster

---
+++ ambari/README.md
@@ -0,0 +1,3 @@
+#Apache Ambari 1.7 cluster Docker image
+
+# IN PROGRESS
---
+++ ambari/bootstrap.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+rm /tmp/*.pid
+
+# disable iptables firewall
+/etc/init.d/iptables save
+/etc/init.d/iptables stop
+
+# Force Ipv6 to stop
+echo 1 > /proc/sys/net/ipv6/conf/all/disable_ipv6
+echo 1 > /proc/sys/net/ipv6/conf/default/disable_ipv6
+
+# sync with time server
+#ntpdate pool.ntp.org
+ntpdate timeserver.svl.ibm.com
+service ntpd start
+
+# start ssh server
+service sshd start
+
+#dnsMask
+service dnsmask start
+
+# start ambari
+/usr/sbin/ambari-server start
+
+#start ambari agent
+#/usr/sbin/ambari-agent start
+
+if [[ $1 = "-d" ]]; then
+  while true; do sleep 1000; done
+fi
+
+if [[ $1 = "-bash" ]]; then
+  /bin/bash
+fi
